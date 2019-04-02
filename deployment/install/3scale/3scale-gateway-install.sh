f16f39cf25b192021e72140d5f3b1db5c8a12b5b
gogs <gogs>
2019-04-02 17:17:24 -0400

    Add Wildcard router for self-managed APICAST

--- deployment/install/3scale/3scale-gateway-install.sh
+++ deployment/install/3scale/3scale-gateway-install.sh
@@ -1,18 +1,48 @@
-THREE_SCALE_GATEWAY_PROJ=3scale-demo-gateway
-APICAST_TEMPLATE=https://raw.githubusercontent.com/3scale/3scale-amp-openshift-templates/2.4.0.GA/apicast-gateway/apicast.yml
+export THREE_SCALE_GATEWAY_PROJ=3scale-demo-gateway
+export APICAST_TEMPLATE=https://raw.githubusercontent.com/3scale/3scale-amp-openshift-templates/2.4.0.GA/apicast-gateway/apicast.yml
 # ENTER THE NAME OF YOUR 3SCALE SaaS TENANT
-SAAS_TENANT_NAME=
+export SAAS_TENANT_NAME=
 # Get the Access Token from Settings Widget->Personal->Tokens
 # You can add a new one with the appropriate permissions
-ACCESS_TOKEN=
+export ACCESS_TOKEN=
+export APICAST_NAME=apicast
 
-oc new-project $THREE_SCALE_GATEWAY_PROJ
+#OPENSHIFT WILDCARD DOMAIN
+export  OCP_ROUTE=apps.sulu.nub3s.io
+export WILDCARD_DOMAIN=$THREE_SCALE_GATEWAY_PROJ.$OCP_ROUTE
 
-# Test to ensure the URL is correct
-curl -k -i https://${ACCESS_TOKEN}@${SAAS_TENANT_NAME}-admin.3scale.net/admin/api/services.json | grep -m 1 "200 OK" || (echo "Access Token Failed" && exit)
+# oc new-project $THREE_SCALE_GATEWAY_PROJ
 
-oc secret new-basicauth apicast-configuration-url-secret --password=https://${ACCESS_TOKEN}@${SASS_TENANT_NAME}-admin.3scale.net
+# # Test to ensure the URL is correct
+# curl -k -i https://${ACCESS_TOKEN}@${SAAS_TENANT_NAME}-admin.3scale.net/admin/api/services.json || grep -m 1 "200 OK" || (echo "Access Token Failed" && exit)
 
-oc new-app -f $APICAST_TEMPLATE -p LOG_LEVEL=debug
+# oc create secret generic apicast-configuration-url-secret --from-literal=password=https://${ACCESS_TOKEN}@${SAAS_TENANT_NAME}-admin.3scale.net  --type=kubernetes.io/basic-auth
 
-# TODO - is Wildcard router possible for self-managed APICAST?
+# oc new-app -f $APICAST_TEMPLATE -p LOG_LEVEL=debug --param APICAST_NAME=$APICAST_NAME
+
+
+until 
+	oc get pods | grep $APICAST_NAME | grep -m 1 "1/1"
+do
+	sleep 2
+done
+
+echo "API Gateway installed. Manually create routes for each API or create a wildcard route"
+
+# IF YOU HAVE CLUSTER ADMIN TO SET WILDCARD ROUTE, UNCOMMENT THE COMMANDS BELOW
+# # UPDATE THE ROUTER TO ALLOW WILDCARD ROUTES
+
+# oc adm router --replicas=0 -n default
+# oc set env dc/router ROUTER_ALLOW_WILDCARD_ROUTES=true -n default
+# oc scale dc/router --replicas=1 -n default
+
+# #   ENSURE THE ROUTER IS RUNNING
+
+# until 
+# 	oc get pods -n default| grep "router" | grep -m 1 "1/1"
+# do
+# 	sleep 2
+# done
+
+# # Create wildcard route for api gateway from the template
+# envsubst < gateway-wildcard-route.yaml.t2 | oc create -f-
