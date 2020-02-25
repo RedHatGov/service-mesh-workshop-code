fecec06174d2476f262e59978e8f455ca6561244
dudash <jason.dudash@gmail.com>
2020-02-24 21:08:46 -0500

    updates to call and render details about the profile service

--- code/app-ui/routes/profile.js
+++ code/app-ui/routes/profile.js
@@ -1,16 +1,29 @@
 var express = require('express')
 var router = express.Router()
+var moment = require('moment')
+var request = require('request-promise')
 
 /* Show users profile page or login/create account */
 router.get('/', function(req, res, next) {
   var user = res.locals.user
-  res.render('profile', { title: 'Your Profile'})
+
+  // TODO: lookup profile for my user ID
+  // TODO: get my profile image
+
+  var fakeme = {
+    aboutMe: 'I know that I am intelligent, because I know that I know nothing',
+    emailAddress: 'jdudash@redhat.com',
+    firstName: 'Jason',
+    lastName: 'Dudash'
+  }
+
+  res.render('profile', { title: "Jason's Profile", profile: fakeme, isMyProfile: true, errorWithProfile: false })
 })
 
 /* GET a user's page. */
 router.get('/:userId', function(req, res, next) {
   var user = res.locals.user
-  const profileGetURI = req.HTTP_PROTOCOL + req.PROFILE_SVC_HOST + ':' + req.PROFILE_SVC_PORT + '/' + req.params.userId 
+  const profileGetURI = req.HTTP_PROTOCOL + req.PROFILE_SVC_HOST + ':' + req.PROFILE_SVC_PORT + '/users/' + req.params.userId 
   req.debug('GET from profile SVC at: ' + profileGetURI)
   var request_get_options = {
       method: 'GET',
@@ -29,24 +42,20 @@ router.get('/:userId', function(req, res, next) {
       json: true // Automatically parses the JSON string in the response
   }
   var profileData = []
+  var profileImage = []
   request(request_get_options)
   .then(function (getresult) {
       req.debug(getresult)  // uncomment to show JSON
-      var profileData = []
-      var itemListPromises = getresult.items.map(function(itemId) { return getItemRequest(req, user, itemId, profileData)});
-      Promise.all(itemListPromises).then(function(profileData) {
-          res.render('profile', { title: 'User Profile', profile: getresult, errorWithProfile: false })
-      })
-      .catch(function (err) {
-          req.debug('ERROR GETTING DATA FROM PROFILE SERVICE')
-          req.debug(err)
-          res.render('profile', { title: 'User Profile', profile:  getresult, errorWithProfile: true })
-      })
+      let title = getresult.firstName + "\'s Profile"
+
+      // TODO: get the profile image
+
+      res.render('profile', { title: title, profile: getresult, isMyProfile: false, errorWithProfile: false })
   })
   .catch(function (err) {
       req.debug('ERROR GETTING DATA FROM PROFILE SERVICE')
       req.debug(err)
-      res.render('profile', { title: 'User Profile', profile:  getresult, errorWithProfile: true })
+      res.render('profile', { title: 'Unknown User', errorWithProfile: true })
   })
 })
 
